21f0bd6f4966c366fe65463e1c509c733f9f3a51
leeozaka <leonardoanicetoneves@hotmail.com>
2025-10-27 20:16:03 -0300

    fix: repositories should not be spring data redis

--- src/main/java/br/com/devfest/codecash/CodecashApplication.java
+++ src/main/java/br/com/devfest/codecash/CodecashApplication.java
@@ -2,9 +2,10 @@ package br.com.devfest.codecash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisRepositoriesAutoConfiguration.class})
 @EnableJpaRepositories(basePackages = "br.com.devfest.codecash.repository")
 public class CodecashApplication {
 
